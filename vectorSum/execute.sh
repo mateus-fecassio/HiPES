6cb9cea840a0991a3d922b983fe6f86905e9e8b9
mateus-decassio <mateusdecassio.ferreira@gmail.com>
2020-04-14 00:32:32 -0300

    vetorizacao e load atemporal

--- vectorSum/execute.sh
+++ vectorSum/execute.sh
@@ -1,13 +1,11 @@
 #!/bin/bash
 
-#Tamanho máximo de valores
-MAX="10"
 
 #Número de repetições
 repetitions="10"
 
 #Escala de valores em x
-x_range="[10:10000]"
+x_range="[1:1024]"
 
 #Resolução de saída dos gráficos gerados pelo gnuplot.
 image_size="1600,900" #1600x900
@@ -15,8 +13,8 @@ image_size="1600,900" #1600x900
 	
 #MEDIÇÃO DE TEMPO
 echo "REALIZANDO O TESTE DE MEDIÇÃO DE TEMPO ..."
-for size in {1..$MAX} ; 
+for size in {1..10} ; 
 	do
-		echo "$size / $MAX (MBytes) done"
+		echo "$size (MBytes) done"
 		./vectorSum -d $size -r $repetitions >> temp.tmp
 	done		
