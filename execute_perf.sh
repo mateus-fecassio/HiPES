56ea8af5c0b73bcaa24b50a4e9ac3a4fc30a6c48
mateus-decassio <mateusdecassio.ferreira@gmail.com>
2020-05-01 15:34:40 -0300

    atualizacoes

--- execute_perf.sh
+++ execute_perf.sh
@@ -1,14 +1,19 @@
 #!/bin/bash
 
+size=100
+repetitions=10
+flags='L1-dcache-loads,L1-dcache-load-misses,L1-dcache-stores,L1-icache-load-misses,LLC-loads,LLC-load-misses,LLC-stores,LLC-store-misses'
+
+
+#------------------------------------------------------------------128
+cd 128
 cd vectorSum1
 make
 
 echo "REALIZANDO O TESTE DE MEDIÇÃO COM PERF PARA vectorSum1 (base)..."
-perf stat -e L1-dcache-loads,L1-dcache-load-misses,L1-dcache-stores,L1-icache-load-misses,LLC-loads,LLC-load-misses,LLC-stores,LLC-store-misses ./vectorSum -d 128 -r 10 >> temp.tmp
+perf stat -e $flags ./vectorSum -d $size -r $repetitions 2>> temp.tmp
 mv ./temp.tmp ../perf_base.tmp
 make purge
-
-
 
 
 cd ..
@@ -16,11 +21,9 @@ cd vectorSum2
 make
 
 echo "REALIZANDO O TESTE DE MEDIÇÃO COM PERF PARA vectorSum2 (vetorizado)..."
-perf stat -e L1-dcache-loads,L1-dcache-load-misses,L1-dcache-stores,L1-icache-load-misses,LLC-loads,LLC-load-misses,LLC-stores,LLC-store-misses ./vectorSum -d 128 -r 10 >> temp.tmp
+perf stat -e $flags ./vectorSum -d $size -r $repetitions 2>> temp.tmp
 mv ./temp.tmp ../perf_vetorizado.tmp
 make purge
-
-
 
 
 cd ..
@@ -28,6 +31,73 @@ cd vectorSum3
 make
 
 echo "REALIZANDO O TESTE DE MEDIÇÃO COM PERF PARA vectorSum3 (vetorizado e nt load)..."
-perf stat -e L1-dcache-loads,L1-dcache-load-misses,L1-dcache-stores,L1-icache-load-misses,LLC-loads,LLC-load-misses,LLC-stores,LLC-store-misses ./vectorSum -d 128 -r 10 >> temp.tmp
+perf stat -e $flags ./vectorSum -d $size -r $repetitions 2>> temp.tmp
 mv ./temp.tmp ../perf_vetorizado_nt_load.tmp
 make purge
+#---------------------------------------------------------------------
+
+
+#------------------------------------------------------------------256
+cd ../..
+cd 256
+cd vectorSum1
+make
+
+echo "REALIZANDO O TESTE DE MEDIÇÃO COM PERF PARA vectorSum1 (base)..."
+perf stat -e $flags ./vectorSum -d $size -r $repetitions 2>> temp.tmp
+mv ./temp.tmp ../perf_base.tmp
+make purge
+
+
+cd ..
+cd vectorSum2
+make
+
+echo "REALIZANDO O TESTE DE MEDIÇÃO COM PERF PARA vectorSum2 (vetorizado)..."
+perf stat -e $flags ./vectorSum -d $size -r $repetitions 2>> temp.tmp
+mv ./temp.tmp ../perf_vetorizado.tmp
+make purge
+
+
+cd ..
+cd vectorSum3
+make
+
+echo "REALIZANDO O TESTE DE MEDIÇÃO COM PERF PARA vectorSum3 (vetorizado e nt load)..."
+perf stat -e $flags ./vectorSum -d $size -r $repetitions 2>> temp.tmp
+mv ./temp.tmp ../perf_vetorizado_nt_load.tmp
+make purge
+#---------------------------------------------------------------------
+
+
+#------------------------------------------------------------------512
+cd ../..
+cd 512
+cd vectorSum1
+make
+
+echo "REALIZANDO O TESTE DE MEDIÇÃO COM PERF PARA vectorSum1 (base)..."
+perf stat -e $flags ./vectorSum -d $size -r $repetitions 2>> temp.tmp
+mv ./temp.tmp ../perf_base.tmp
+make purge
+
+
+cd ..
+cd vectorSum2
+make
+
+echo "REALIZANDO O TESTE DE MEDIÇÃO COM PERF PARA vectorSum2 (vetorizado)..."
+perf stat -e $flags ./vectorSum -d $size -r $repetitions 2>> temp.tmp
+mv ./temp.tmp ../perf_vetorizado.tmp
+make purge
+
+
+cd ..
+cd vectorSum3
+make
+
+echo "REALIZANDO O TESTE DE MEDIÇÃO COM PERF PARA vectorSum3 (vetorizado e nt load)..."
+perf stat -e $flags ./vectorSum -d $size -r $repetitions 2>> temp.tmp
+mv ./temp.tmp ../perf_vetorizado_nt_load.tmp
+make purge
+#---------------------------------------------------------------------
