1a7b3b01011b924b9fa0c65a284d8b1a01fedd14
mateus-decassio <mateusdecassio.ferreira@gmail.com>
2020-08-26 14:33:42 -0300

    atualizacoes

--- execute-pred.sh
+++ execute-pred.sh
@@ -10,22 +10,7 @@ INIT=1
 #fim
 END=2
 
-flags='
-cache-misses,cpu-cycles,
-L1-dcache-loads,
-L1-dcache-load-misses,
-L1-dcache-stores,
-L1-icache-load-misses,
-LLC-loads,
-LLC-load-misses,
-LLC-stores,
-LLC-store-misses,
-mem_load_retired.l1_hit,
-mem_load_retired.l1_miss,
-mem_load_retired.l2_hit ,
-mem_load_retired.l2_miss,
-mem_load_retired.l3_hit,
-mem_load_retired.l3_miss'
+flags='cache-misses,cpu-cycles,L1-dcache-loads,L1-dcache-load-misses,L1-dcache-stores,L1-icache-load-misses,LLC-loads,LLC-load-misses,LLC-stores,LLC-store-misses,mem_load_retired.l1_hit,mem_load_retired.l1_miss,mem_load_retired.l2_hit ,mem_load_retired.l2_miss,mem_load_retired.l3_hit,mem_load_retired.l3_miss'
 
 leng=1023
 
